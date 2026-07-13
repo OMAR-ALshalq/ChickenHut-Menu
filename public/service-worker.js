86fc96b663b7c438c814bbd0e105298fca25e7f4
OMAR-ALshalq <omaralshalq@gmail.com>
2026-07-14 01:56:57 +0300

    edit App

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "chickenhut-v1";
+const urlsToCache = ["/", "/index.html", "/manifest.json", "/Logo.jpg"];
+
+// تثبيت Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// استجابة من الكاش أو الشبكة
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
